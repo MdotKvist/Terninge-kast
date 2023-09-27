bba9544a811b4cf89493b98921916a07c7285444
MdotKvist <Kvist339@gmail.com>
2023-09-27 12:01:05 +0200

    Update Terninge kast.cpp

--- Terninge kast/Terninge kast.cpp
+++ Terninge kast/Terninge kast.cpp
@@ -2,11 +2,10 @@
 #include <ctime>
 #include <cstdlib>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
- 
+
 
 int main()
 {
@@ -16,21 +15,33 @@ int main()
     cout << "Terninge spil\n\n";
 
     int AntalT;
-        cout << "Hvor mange terninger vil du spille med? ";
-        cin >> AntalT;
+    cout << "Hvor mange terninger vil du spille med? ";
+    cin >> AntalT;
 
-        vector<int> resultater;
+    vector<int> resultater;
 
-        for (int i = 0; i < AntalT; i++)
+    for (int i = 0; i < AntalT; i++)
+    {
+        int random = (rand() % 6) + 1;
+        resultater.push_back(random);
+    }
+
+    for (int i = 1; i < resultater.size(); i++)
+    {
+        int current = resultater[i];
+        int j = i - 1;
+
+        while (j >= 0 && resultater[j] > current)
         {
-            int random = (rand() % 6) + 1;
-            resultater.push_back(random);
+            resultater[j + 1] = resultater[j];
+            j--;
         }
 
-        sort(resultater.begin(), resultater.end());
+        resultater[j + 1] = current;
+    }
 
-        for (int resul : resultater) {
-            cout << resul << "\n";
-        }
+    for (int resul : resultater) {
+        cout << resul << "\n";
+    }
 
 }
